daa36d8be1780b03c47ddd4b61628a8d3cf9fc63
luishhmnn <hohmann.luis@web.de>
2021-10-21 11:12:03 +0200

    Move training data generatrion into ADNAforAirSim_CNN project

--- experiments/get_training_data/get_training_data.bat
+++
@@ -1,7 +0,0 @@
-start "AirSim CityEnviron" "..\..\AirSimServer\CityEnviron\WindowsNoEditor\CityEnviron.exe" --settings "%~dp0\airsim_settings.json"
-
-start "ADNAforAirSim" "..\..\Client\build\x64\Debug\ADNAAirSimClient.exe"
-
-set "DNAPATH=%~dp0get_training_data"
-set "DNAPATH=%DNAPATH:\=\\%"
-start "GPP 1" "GeneralPurposeProcessor.exe" /DNA:%DNAPATH% 1
--- experiments/get_training_data/start_airsim.bat
+++
@@ -1,1 +0,0 @@
-start "AirSim CityEnviron" "..\..\AirSimServer\CityEnviron\WindowsNoEditor\CityEnviron.exe" --settings "%~dp0\airsim_settings.json"
